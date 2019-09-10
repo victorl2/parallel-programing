282741946af3c267efaa87a80219e1508fa56687
phgeraldeli@id.uff.br <email@example>
2019-09-10 08:32:08 -0300

    exemplo com mpi

---
+++ hello-world-mpi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+
+/**
+ * mpicc teste.c -o test
+ * mpirun -np 5 test
+ */
+int main(int argc, char** argv){
+    int meu_rank, np, origem, destino, tag=0;
+    char msg[100];
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&meu_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&np);
+
+    if(meu_rank != 0){
+        sprintf(msg, "Processo %d está vivo!", meu_rank);
+        destino = 0;
+        MPI_Send(msg, strlen(msg)+1,MPI_CHAR,destino,tag,MPI_COMM_WORLD);
+    }else{
+        for(origem = 1; origem < np; origem++) {
+            MPI_Recv(msg, 100, MPI_CHAR, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+            printf("%s\n", msg);
+        }
+    }
+
+    printf("Execução do processo %d foi concluida\n", meu_rank);
+    MPI_Finalize();
+    return 0;
+}
